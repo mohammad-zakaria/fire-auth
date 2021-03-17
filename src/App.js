c23b173c7c4b43841ce0e6c8367c852a5e19217b
Mohammad Zakaria <zakaria.eee.iiuc@gmail.com>
2021-03-17 21:47:35 +0600

    before update state

--- src/App.js
+++ src/App.js
@@ -49,6 +49,22 @@ function App() {
       // An error happened.
     });
   }
+  const handleChange = (event) =>{
+    if(event.target.name === "email"){
+      const isEmailValid = /\S+@\S+\.\S+/.test(event.target.value);
+      console.log(isEmailValid);
+    }
+    if(event.target.name === 'password'){
+      const isPasswordValid = event.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(event.target.value);
+      console.log(isPasswordValid && passwordHasNumber)
+
+    }
+    console.log(event.target.name, event.target.value);
+  }
+  const handleSubmit = () => {
+
+  }
   return (
     <div className="App">
       {
@@ -63,6 +79,14 @@ function App() {
             <img src={user.photo} alt=""/>
           </div>
       }
+      <h1>Our own authentication</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="email" onBlur={handleChange} placeholder="Enter Your Email" required/>
+        <br/>
+        <input type="password" name="password" onBlur={handleChange} placeholder="Enter Your Password" required/>
+        <br/>
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
   );
 }
